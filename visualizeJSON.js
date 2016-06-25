5dcce349eca2fd2eeecc520b567a2154cc481636
sach <sacha.saint-leger@mail.mcgill.ca>
2016-06-25 13:05:05 -0400

    polling 2nd referendum votes every 10 seconds

--- visualizeJSON.js
+++ visualizeJSON.js
@@ -8,8 +8,15 @@ function preload() {
 function setup() {
     createCanvas(480, 120);
     console.log(secondRefData.data.attributes.signature_count);
+    frameRate(0.1);
+
 }
 
 function draw() {
+    loadJSON("https://petition.parliament.uk/petitions/131215.json", totalSignatures);
+}
+
+function totalSignatures(secondRefData) {
+    console.log(secondRefData.data.attributes.signature_count);
 }
 
