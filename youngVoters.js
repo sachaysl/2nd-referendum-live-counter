076035ac4dd49637e71e6e56af96becbb8254a39
sach <sacha.saint-leger@mail.mcgill.ca>
2016-07-02 23:35:51 -0400

    added young voters that didn't vote

--- youngVoters.js
+++ youngVoters.js
@@ -29,5 +29,58 @@ function youngVoters() {
 	    .attr("xlink:href","resources/fullManGrey.jpg");
     }
 
+    //add youngVotersWhoDidntVote
+    //2.9m remain, 0.9m leave
+    //four rows of 1m --> 5 men
+
+    var youngVotersNoVote = svg.append('g').attr('id', '#youngVotersNoVote')
+
+    for (var i=0; i<5; i++) {
+	youngVotersNoVote.append('svg:image')
+	    .attr('x', 90 + (i * 25))
+	    .attr('y', 420)
+	    .attr('width', 32)
+	    .attr('height', 54)
+	    .attr("xlink:href","resources/fullManGreen.jpg");
+    }
+
+    for (var i=0; i<4; i++) {
+	youngVotersNoVote.append('svg:image')
+	    .attr('x', 90 + (i * 25))
+	    .attr('y', 480)
+	    .attr('width', 32)
+	    .attr('height', 54)
+	    .attr("xlink:href","resources/fullManGreen.jpg");
+    }
+
+    youngVotersNoVote.append('svg:image')
+	.attr('x', 85 + (4 * 25))
+	.attr('y', 480)
+	.attr('width', 32)
+	.attr('height', 54)
+	.attr("xlink:href","resources/halfManGreen.jpg");
+
+    for (var i=0; i<4; i++) {
+	youngVotersNoVote.append('svg:image')
+	    .attr('x', 90 + (i * 25))
+	    .attr('y', 540)
+	    .attr('width', 32)
+	    .attr('height', 54)
+	    .attr("xlink:href","resources/fullManGrey.jpg");
+    }
+
+    youngVotersNoVote.append('svg:image')
+	.attr('x', 85 + (4 * 25))
+	.attr('y', 540)
+	.attr('width', 32)
+	.attr('height', 54)
+	.attr("xlink:href","resources/halfManGrey.jpg");
+
+
+    
+
+
+    
+    
     
 }
