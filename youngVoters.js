b6d796a57bf8f3f0677659d8b2522ce7ca8d3ec3
sach <sacha.saint-leger@mail.mcgill.ca>
2016-07-02 22:38:04 -0400

    added leave votes

--- youngVoters.js
+++ youngVoters.js
@@ -1,7 +1,8 @@
 function youngVoters() {
 
-    var svg = d3.select("#sketch").attr("width", 1200);
+    var svg = d3.select("#sketch").attr("width", 1200).attr("height", 800);
 
+    //add remain votes
     for (var i=0; i< 32; i++) {
 	svg.append('svg:image')
 	    .attr('x', 90 + (i * 25))
@@ -10,4 +11,15 @@ function youngVoters() {
 	    .attr('height', 54)
 	    .attr("xlink:href","resources/fullManGreen.jpg");
     }
+
+    for (var i=0; i< 36; i++) {
+	svg.append('svg:image')
+	    .attr('x', 90 + (i * 25))
+	    .attr('y', 120)
+	    .attr('width', 32)
+	    .attr('height', 54)
+	    .attr("xlink:href","resources/fullManGrey.jpg");
+    }
+
+    
 }
