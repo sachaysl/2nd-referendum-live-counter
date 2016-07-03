2ff076e016abf0854fe82ae472e6a8da1e1312f6
sach <sacha.saint-leger@mail.mcgill.ca>
2016-07-03 10:43:45 -0400

    added title

--- youngVoters.js
+++ youngVoters.js
@@ -1,10 +1,18 @@
 function youngVoters() {
 
     var svg = d3.select("#sketch").attr("width", 1200).attr("height", 800);
+    var votes = svg.append('g').attr('id', '#votes');
 
+    svg.append('text')
+	.attr('x',100)
+	.attr('y',0)
+	.text('The Case For a Second EU Referendum')
+	.style('font-size', '1.5rem')
+	.style('font-family', 'Gill Sans');
+	
     //add remain votes
     for (var i=0; i< 32; i++) {
-	svg.append('svg:image')
+	votes.append('svg:image')
 	    .attr('x', 90 + (i * 25))
 	    .attr('y', 60)
 	    .attr('width', 30)
@@ -12,7 +20,7 @@ function youngVoters() {
 	    .attr('xlink:href','resources/fullManGreen.jpg');
     }
 
-    svg.append('svg:image')
+    votes.append('svg:image')
 	.attr('x', 85 + (32*25))
 	.attr('y', 60)
 	.attr('width', 32)
@@ -21,7 +29,7 @@ function youngVoters() {
 
     //add leave votes
     for (var i=0; i< 36; i++) {
-	svg.append('svg:image')
+	votes.append('svg:image')
 	    .attr('x', 90 + (i * 25))
 	    .attr('y', 120)
 	    .attr('width', 32)
