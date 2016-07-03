4abed30ec8d645d06a16e83ec2e092bc8d627262
sach <sacha.saint-leger@mail.mcgill.ca>
2016-07-02 22:36:10 -0400

    added remain votes

--- youngVoters.js
+++ youngVoters.js
@@ -1,11 +1,13 @@
 function youngVoters() {
 
-    var svg = d3.select("#sketch");
+    var svg = d3.select("#sketch").attr("width", 1200);
 
-    svg.append('svg:image')
-	.attr('x',90)
-	.attr('y', 60)
-	.attr('width', 32)
-	.attr('height', 54)
-	.attr("xlink:href","resources/fullManGreen.jpg");
+    for (var i=0; i< 32; i++) {
+	svg.append('svg:image')
+	    .attr('x', 90 + (i * 25))
+	    .attr('y', 60)
+	    .attr('width', 32)
+	    .attr('height', 54)
+	    .attr("xlink:href","resources/fullManGreen.jpg");
+    }
 }
